736219d314d1f1026c383e1d7d8b09da2b688600
Vasil <vasilgm@yandex.ru>
2023-04-21 18:43:18 +0300

    Ex1 mod

--- Ex1/Program.cs
+++ Ex1/Program.cs
@@ -1,3 +1,22 @@
 ﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
 // N = 5 -> "5, 4, 3, 2, 1"
 // N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+double number (string i)
+{
+    System.Console.Write($"Введите значение {i} > ");
+    return double.Parse(Console.ReadLine());
+}
+
+double max = number ("max");
+double min = number ("min");
+outputNumber(max, min);
+
+void outputNumber(double max, double min)
+{
+    if (max > 0 ) 
+    {
+        System.Console.WriteLine(max);
+        outputNumber(max-1,min);
+    }
+}
